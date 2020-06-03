e8851d47506d326acb3f19d33f8890b096ef9a9c
Justin <justin.kay@inxpress.com>
2020-06-03 08:05:18 -0600

    Fix the call to retrieve the store_id on the register page

--- Controller/Adminhtml/Register/Index.php
+++ Controller/Adminhtml/Register/Index.php
@@ -49,7 +49,7 @@ class Index extends Action
         $resultRedirect = $this->resultFactory->create(ResultFactory::TYPE_REDIRECT);
 
         $storeScope = \Magento\Store\Model\ScopeInterface::SCOPE_STORES;
-        $store_id = $this->_scopeConfig->getValue("system/carriers/dhlexpress/store_id", $storeScope);
+        $store_id = $this->_scopeConfig->getValue("system/carriers/dhlexpress/store_id", $storeScope, \Magento\Store\Model\Store::DEFAULT_STORE_ID);
 
         $params = $this->_request->getParams();
 
